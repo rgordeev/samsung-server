a6e4c6f4eac1d70a71a123054978729472fd21e2
Roman Gordeev <roma.gordeev@gmail.com>
2019-12-03 16:56:59 +0300

    change controller mapping

--- src/main/java/ru/rgordeev/server/controllers/APIController.java
+++ src/main/java/ru/rgordeev/server/controllers/APIController.java
@@ -8,7 +8,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
-@RestController("/api")
+@RestController("api")
+@RequestMapping("/api")
 public class APIController {
 
     @Getter
